76babbd49a954da6a88ef46daf2254e4f32c1fcf
Hendrik Schaeidt <he.schaeidt@gmail.com>
2015-11-20 12:45:55 +0100

    Add Sonata admin bundle to project
    * Registration on AppKernel and composer
    * Add sonata configuration to app/config.yml
    * Add sonata admin routing to app/routing.yml
    * Installing assets through app/console assets:install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,14 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            //Sonata start
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            //Sonata end
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
