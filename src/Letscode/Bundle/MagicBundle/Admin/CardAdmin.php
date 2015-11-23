de6b611b5f04885036a1bff1e71c061f8fe2bc0b
Hendrik Schaeidt <he.schaeidt@gmail.com>
2015-11-23 22:41:07 +0100

    Add base proof of concept of automatic description parsing.
    * Add description to entity
    * Add setter for entityManager in CardAdmin
    * Inject entityManager to CardAdmin via services
    * Add regex example how to parse description and map to attributes
    * Add new attributes to Card entity in prePersist
    * Add a check for existing attributes on the card (no doubles allowed)

--- src/Letscode/Bundle/MagicBundle/Admin/CardAdmin.php
+++ src/Letscode/Bundle/MagicBundle/Admin/CardAdmin.php
@@ -2,17 +2,48 @@
 
 namespace Letscode\Bundle\MagicBundle\Admin;
 
+use Letscode\Bundle\MagicBundle\Entity\Attribute;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Model\ModelManagerInterface;
 
 class CardAdmin extends Admin
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @var array
+     */
+    private $descriptionPatterns = [
+        '/put.*[^\+]\d\/[^\+]\d.*token/i' => ['Creature', 'Token']
+    ];
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->entityManager;
+    }
+
+    /**
+     * @param \Doctrine\ORM\EntityManager $entityManager
+     */
+    public function setEntityManager($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper->with("General")
         ->add('name', 'text')
+        ->add('description', 'text')
         ->add('convertedManaCost', 'integer')
         ->add(
             'manaCosts', 'sonata_type_model', array(
@@ -68,6 +99,39 @@ class CardAdmin extends Admin
         ->with('Creature')
             ->add('power', 'integer', array('required' => false))
             ->add('toughness', 'integer', array('required' => false));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function prePersist($object)
+    {
+        $result = null;
+        $guessedAttribute = '';
+        $em = $this->getEntityManager();
+        $description = $object->getDescription();
+
+        foreach ($this->descriptionPatterns as $pattern => $attributes) {
+            //if (preg_match($pattern, $description) == 1) {
+                foreach ($attributes as $attribute) {
+                    $guessedAttribute = $attribute;
+
+                    $result = $em->getRepository('LetscodeMagicBundle:Attribute')->findOneBy(array(
+                        'name' => $guessedAttribute
+                    ));
+
+                    if ($result == null) {
+                        $result = new Attribute();
+                        $result->setName($guessedAttribute);
+                        $em->persist($result);
+                    }
+
+                    if (!$object->hasAttribute($result)) {
+                        $object->addAttribute($result);
+                    }
+                }
+            //}
+        }
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
